43c15b38549ed9329be43e0845e145f84a1b22a8
Vitor <vfarias@bionexo.com>
2021-09-26 14:05:36 -0400

    remove models dont using

--- src/app/models/authResult.ts
+++
@@ -1,8 +0,0 @@
-import { User } from "./user";
-
-export class AuthResult
-{    
-    public accessToken!: string;
-    public user!: User;
-
-}
--- src/app/template/navbar/navbar.component.ts
+++ src/app/template/navbar/navbar.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-navbar',
